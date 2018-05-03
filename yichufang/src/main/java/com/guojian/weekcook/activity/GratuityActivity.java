5ca031e52933f631b2c151e96f8075eec20bb95a
jkloshhm <jkloshhm@126.com>
2018-05-03 23:31:06 +0800

    打赏界面添加

--- yichufang/src/main/java/com/guojian/weekcook/activity/GratuityActivity.java
+++ yichufang/src/main/java/com/guojian/weekcook/activity/GratuityActivity.java
@@ -2,6 +2,8 @@ package com.guojian.weekcook.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.guojian.weekcook.R;
 
@@ -10,11 +12,26 @@ import com.guojian.weekcook.R;
  *
  * @author jack.guo 2018-02-12
  */
-public class GratuityActivity extends AppCompatActivity {
+public class GratuityActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView mBackButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gratuity);
+        mBackButton = findViewById(R.id.bn_back_gratuity);
+        mBackButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.bn_back_gratuity:
+                finish();
+                break;
+            default:
+                break;
+        }
     }
 }
