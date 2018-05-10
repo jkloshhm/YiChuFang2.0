46ee8a7be26dc0911a1bf62ae7257572decc86ef
jkloshhm <jkloshhm@126.com>
2018-05-10 23:12:35 +0800

    打赏界面添加

--- yichufang/src/main/java/com/guojian/weekcook/activity/GratuityActivity.java
+++ yichufang/src/main/java/com/guojian/weekcook/activity/GratuityActivity.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.guojian.weekcook.R;
 
@@ -22,8 +22,9 @@ public class GratuityActivity extends AppCompatActivity implements View.OnClickL
     private RadioButton mWeChatButton;
     private ImageView mAlipayQr;
     private ImageView mWeChatQr;
+    private TextView mWeChatQrTips;
+    private TextView mAlipayQrTips;
     private Button mSaveImageButton;
-    private RadioGroup mRadioGroup;
 
 
     @Override
@@ -35,35 +36,13 @@ public class GratuityActivity extends AppCompatActivity implements View.OnClickL
         mWeChatButton = findViewById(R.id.btn_wechat);
         mAlipayQr = findViewById(R.id.iv_alipay_qr);
         mWeChatQr = findViewById(R.id.iv_wechat_qr);
+        mWeChatQrTips = findViewById(R.id.tv_wechat_qr_tips);
+        mAlipayQrTips = findViewById(R.id.tv_alipay_qr_tips);
         mSaveImageButton = findViewById(R.id.btn_save_image);
-        mRadioGroup = findViewById(R.id.radioGroup_qr_btn);
         mBackButton.setOnClickListener(this);
         mAlipayButton.setOnClickListener(this);
         mWeChatButton.setOnClickListener(this);
         mSaveImageButton.setOnClickListener(this);
-        /*mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
-                switch (checkedId) {
-                    case R.id.btn_alipay:
-                        //支付宝
-                        //mAlipayButton.setChecked(true);
-                        //mWeChatButton.setChecked(false);
-                        mAlipayQr.setVisibility(View.VISIBLE);
-                        mWeChatQr.setVisibility(View.GONE);
-                        break;
-                    case R.id.btn_wechat:
-                        //微信
-                        //mAlipayButton.setChecked(false);
-                        //mWeChatButton.setChecked(true);
-                        mAlipayQr.setVisibility(View.GONE);
-                        mWeChatQr.setVisibility(View.VISIBLE);
-                        break;
-                    default:
-                        break;
-                }
-            }
-        });*/
     }
 
     @Override
@@ -72,19 +51,19 @@ public class GratuityActivity extends AppCompatActivity implements View.OnClickL
             case R.id.btn_back_gratuity:
                 finish();
                 break;
-            case R.id.btn_alipay:
-                //支付宝
-                //mAlipayButton.setChecked(true);
-                //mWeChatButton.setChecked(false);
-                mAlipayQr.setVisibility(View.VISIBLE);
-                mWeChatQr.setVisibility(View.GONE);
-                break;
             case R.id.btn_wechat:
                 //微信
-                //mAlipayButton.setChecked(false);
-                //mWeChatButton.setChecked(true);
                 mAlipayQr.setVisibility(View.GONE);
                 mWeChatQr.setVisibility(View.VISIBLE);
+                mAlipayQrTips.setVisibility(View.GONE);
+                mWeChatQrTips.setVisibility(View.VISIBLE);
+                break;
+            case R.id.btn_alipay:
+                //支付宝
+                mAlipayQr.setVisibility(View.VISIBLE);
+                mWeChatQr.setVisibility(View.GONE);
+                mAlipayQrTips.setVisibility(View.VISIBLE);
+                mWeChatQrTips.setVisibility(View.GONE);
                 break;
             case R.id.btn_save_image:
                 //保存照片
