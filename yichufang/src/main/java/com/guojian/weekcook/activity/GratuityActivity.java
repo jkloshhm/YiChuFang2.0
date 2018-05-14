71fcc91e144ae237c653963178ed2a7cb050bd1a
jkloshhm <jkloshhm@126.com>
2018-05-14 17:21:40 +0800

    1. 打赏功能按钮修改

--- yichufang/src/main/java/com/guojian/weekcook/activity/GratuityActivity.java
+++ yichufang/src/main/java/com/guojian/weekcook/activity/GratuityActivity.java
@@ -1,14 +1,25 @@
 package com.guojian.weekcook.activity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.guojian.weekcook.R;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * 打赏Activity
@@ -69,15 +80,47 @@ public class GratuityActivity extends AppCompatActivity implements View.OnClickL
                 mIsWechat = false;
                 break;
             case R.id.btn_save_image:
-                //保存照片
-                if(mIsWechat){
-
-                }else{
-
+                //保存二维码图片
+                if (mIsWechat) {
+                    saveImageToAlbum(R.mipmap.qr_wechat, "微信二维码保存成功~");
+                } else {
+                    saveImageToAlbum(R.mipmap.qr_alipay, "支付宝二维码保存成功~");
                 }
                 break;
             default:
                 break;
+        }
+    }
+
+    private void saveImageToAlbum(int res, String toast) {
+        //Bitmap mBitmap = ((BitmapDrawable) getResources().getDrawable(res)).getBitmap();
+        Bitmap mBitmap = BitmapFactory.decodeResource(getResources(), res);
+
+
+        String path = Environment.getExternalStorageDirectory() + File.separator + Environment.DIRECTORY_DCIM + File.separator + "Camera";
+        File fileDir = new File(path);
+        if (!fileDir.exists()) {
+            fileDir.mkdir();
+        }
+        String fileName = "YiChuFang" + new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date(System.currentTimeMillis())) + ".png";
+        File file = new File(path, fileName);
+        FileOutputStream fileOutputStream = null;
+        try {
+            fileOutputStream = new FileOutputStream(file);
+            mBitmap.compress(Bitmap.CompressFormat.PNG, 100, fileOutputStream);
+            Uri uri = Uri.fromFile(file);
+            sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, uri));
+            Toast.makeText(this, toast, Toast.LENGTH_SHORT).show();
+
+            fileOutputStream.flush();
+            fileOutputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, toast, Toast.LENGTH_SHORT).show();
+        } finally {
+            if (mBitmap != null) {
+                mBitmap.recycle();
+            }
         }
     }
 }
