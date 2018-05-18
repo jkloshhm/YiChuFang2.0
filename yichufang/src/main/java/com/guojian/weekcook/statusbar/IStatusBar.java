3ac8a0086f138ada6ecf6a2834893e35718c1422
jkloshhm <jkloshhm@126.com>
2018-05-18 13:26:25 +0800

    一些配置上传

--- yichufang/src/main/java/com/guojian/weekcook/statusbar/IStatusBar.java
+++ yichufang/src/main/java/com/guojian/weekcook/statusbar/IStatusBar.java
@@ -4,15 +4,28 @@ import android.app.Activity;
 import android.view.Window;
 
 /**
- * Created by jack.guo on 2018/1/15.
  * 状态栏 根据版本不同使用不同的实现方式
+ *
+ * @author jack.guo on 2018/1/15.
  * @see StatusBarLollipopImpl
  * @see StatusBarMImpl
  */
 
 public interface IStatusBar {
-    //设置状态栏颜色
+    /**
+     * 设置状态栏颜色
+     *
+     * @param window the window
+     * @param color  status bar color
+     */
     void setStatusBarColor(Window window, int color);
-    //设置状态栏全透明，顶部布局会顶上去
+
+    /**
+     * 设置状态栏全透明，顶部布局会顶上去
+     *
+     * @param activity                activity
+     * @param hideStatusBarBackground 是否隐藏状态栏背景
+     * @return 状态栏是否透明
+     */
     boolean translucentStatusBar(Activity activity, boolean hideStatusBarBackground);
 }
