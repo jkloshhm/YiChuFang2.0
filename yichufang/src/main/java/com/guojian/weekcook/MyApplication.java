e1e30fbfc3e117a44d340cfd3808fd260d92fa64
jkloshhm <jkloshhm@126.com>
2018-05-18 12:04:25 +0800

    一些配置上传

--- yichufang/src/main/java/com/guojian/weekcook/MyApplication.java
+++ yichufang/src/main/java/com/guojian/weekcook/MyApplication.java
@@ -1,22 +1,14 @@
 package com.guojian.weekcook;
 
-import android.app.Application;
-
 import com.mob.MobSDK;
-import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
-import com.nostra13.universalimageloader.cache.memory.impl.LruMemoryCache;
-import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
-import com.tencent.bugly.Bugly;
+
 import com.tencent.tinker.loader.app.TinkerApplication;
 import com.tencent.tinker.loader.shareutil.ShareConstants;
 
 /**
- * Created by guojian on 11/16/16.
- * @author jack.guo
- *
- * @ 自定义的MyApplication类
+ * @author Created by jkloshhm on 11/16/16.
+ * <p>
+ * MyApplication: 自定义的MyApplication类
  */
 public class MyApplication extends TinkerApplication {
 
